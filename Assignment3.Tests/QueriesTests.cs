ed5b95be889ca3d039a0ef88907fc5d8d752623a
Rasmus-Balthazar <74965526+Rasmus-Balthazar@users.noreply.github.com>
2021-09-28 14:29:46 +0200

    Add first 3 queries in linq (#4)
    They are still missing extension methods. outcommented part 4 due to it not compiling and wanting to share it so that you can work on it as well

--- Assignment3.Tests/QueriesTests.cs
+++ Assignment3.Tests/QueriesTests.cs
@@ -1,6 +1,6 @@
 using System;
 using Xunit;
-
+using System.Collections.Generic;
 
 namespace BDSA2021.Assignments03.Tests
 
@@ -8,9 +8,38 @@ namespace BDSA2021.Assignments03.Tests
     public class QueriesTests
     {
         [Fact]
-        public void Test1()
+        public void Test_FindWizardsByAuthor_WhereAuthorIsRowling()
         {
+            IEnumerable<string> expected = new List<string>(){"Sirius Black"};
 
+            var actual = Queries.FindWizardsByAuthor("J.K. Rowling");
+
+            Assert.Equal(expected, actual);  
+        }
+
+        [Fact]
+        public void Test_FindTheYear_TheFirstSith_ContainingDarth_WasIntroduced()
+        {
+        //Given
+        int? expected = 1977;
+        //When
+        var actual = Queries.FindYearOfFirstSith();
+        //Then
+        Assert.Equal(expected, actual);  
+        }
+
+        [Fact]
+        public void Test_FindUniqueWizardsFromHPBooks_ReturnedAsTupleList()
+        {
+        //Given
+        var expected = new List<(string name, int? year)>
+        {
+            ("Sirius Black", 1999)
+        }; 
+        //When
+        var actual = Queries.FindUniqueWizardsFromHarryPotter();
+        //Then
+        Assert.Equal(expected, actual);
         }
     }
 }
--- Assignment3.Tests/WizardTests.cs
+++ Assignment3.Tests/WizardTests.cs
@@ -1,19 +1,11 @@
 using Xunit;
-
-
+using System.Linq;
+using System.Collections.Generic;
 namespace BDSA2021.Assignments03.Tests
 
 {
     public class WizardTests
     {
-        [Fact]
-        public void Wizards_contains_2_wizards()
-        {
-            var wizards = Wizard.Wizards.Value;
-
-            Assert.Equal(2, wizards.Count);
-        }
-
         [Theory]
         [InlineData("Darth Vader", "Star Wars", 1977, "George Lucas")]
         [InlineData("Sauron", "The Fellowship of the Ring", 1954, "J.R.R. Tolkien")]
